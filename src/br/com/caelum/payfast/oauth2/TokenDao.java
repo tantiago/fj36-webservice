1c469025f1f44cf237eaeba56ba2c9d2517b3af0
Tiago T Soares <tantiago@gmail.com>
2017-09-09 15:09:00 -0300

    Exericicio 9.6

--- src/br/com/caelum/payfast/oauth2/TokenDao.java
+++ src/br/com/caelum/payfast/oauth2/TokenDao.java
@@ -8,7 +8,16 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class TokenDao {
 	
+	private List<String> authorizationCodes = new ArrayList<>();
 	private List<String> accessTokens = new ArrayList<>();
+	
+	public void adicionaAuthorizationCode(String code) {
+		authorizationCodes.add(code);
+	}
+	
+	public boolean existeAuthorizationCode(String code) {
+		return authorizationCodes.contains(code);
+	}
 	
 	public void adicionaAccessToken(String token) {
 		System.out.println("Adicionando token " + token);
